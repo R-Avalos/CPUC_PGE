ca3eb918940c2978a3f638c4685465f723e665df
Randy Avalos <randy.joseph.avalos@gmail.com>
2016-05-02 18:49:42 -0700

    Small update to sender name/email split

--- CPUC_PGE.r
+++ CPUC_PGE.r
@@ -25,11 +25,17 @@ dest_file <- getURL("ftp://ftp2.cpuc.ca.gov/PG&E20150130ResponseToA1312012Ruling
 email_index <- fread(dest_file, sep = ",", header = TRUE) #convert to data frame
 # save(email_index, file = "email_index.rda") # save file to local
 load("email_index.rda")
-
+remove(email_index)
 SanBruno <- ymd("2010-09-09") #San Bruno explosion
 
 # Transform ###############################
-email_index$MasterDate <- mdy_hm(email_index$MasterDate) # convet to Date
+email_index$Recipient[email_index$Recipient==""] <- "Not Recorded" #stand in for missing receipent value
+email_index$Sender[email_index$Sender==""] <- "Not Recorded"
+email_index$Subject[email_index$Subject==""] <- "Not Recorded"
+## these values may refere to email one line up in data frame
+
+email_index$MasterDate <- mdy_hm(email_index$MasterDate) # convet to Date, 136 failed to parse
+
 
 email_index$YearMonthDay <- format(as.POSIXct(email_index$MasterDate, format="%Y-%m-%d"), format="%Y-%m-%d") #remove hours, minutes, seconds
 email_index$YearMonthDay <- ymd(email_index$YearMonthDay) #convert to date
@@ -54,4 +60,6 @@ plotEmail
 
 x <- strsplit(email_index$Sender, "\\(") #seperate name and email... have to deal with blank and other
 head(x)
-x <- matrix(unlist(x), ncol=2, byrow=TRUE)
+x <- matrix(unlist(x), ncol=2, byrow=TRUE) #doesn't work
+x.df <- as.data.frame(x) #convert to data frame
+x.df <- rename(x.df, sender_name = V1, sender_email = V2) #note how we only have 93267 observation of origional 120k .. :/
