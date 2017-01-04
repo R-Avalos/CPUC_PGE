d8d74a218121c73e21c6636a7fd54141c567cc74
Randy Avalos <randy.joseph.avalos@gmail.com>
2017-01-03 20:26:27 -0800

    Created method to combine into data frame. Cleaned up text a bit. First visual of word count.

--- CPUC_pdfdownload.r
+++ CPUC_pdfdownload.r
@@ -3,6 +3,7 @@
 library(RCurl)
 library(lubridate)
 library(dplyr)
+library(tibble)
 library(feather)
 library(tm)
 # library(httr) best used for API
@@ -29,7 +30,7 @@ downloadPDF <- function(x) {
 }
 
 ### Function specific to this FTP site to download files
-# Function will first creaet the folder in the working directory for the year month
+# Function will first create the folder in the working directory for the year month
 # Once that is created, the function will select the PDF files (removing the excel files) and download the pdfs to the loca directory
 
 FTP_downloadandstore_pdf_func <- function(ftp_url = url, year = 2010, month_in_digits = 01) {
@@ -67,7 +68,7 @@ FTP_downloadandstore_pdf_func <- function(ftp_url = url, year = 2010, month_in_d
 }
 
 
-# Function to download files to local storage
+# Call Function to download files to local storage
 FTP_downloadandstore_pdf_func(year = 2010, month_in_digits = 02)
 
 
@@ -76,41 +77,48 @@ FTP_downloadandstore_pdf_func(year = 2010, month_in_digits = 02)
 
 
 dummy <- function(file_folder){
-        myfiles <- list.files(path = file_folder, pattern = "pdf", full.names = TRUE)
-        #convert to pdf
+        library(readr)
+        myfiles <- list.files(path = file_folder, pattern = "pdf", full.names = TRUE) # list files
+        lapply(myfiles, function(i) system(paste('"C:/Program Files/xpdf/bin64/pdftotext.exe"', paste0('"', i, '"')), wait = FALSE) ) # convert to pdf
         
         # add text files to dataframe
         mytxtfiles <- list.files(path = file_folder, pattern = "txt", full.names = TRUE)
-        for(i in mytxtfilesx){
-        #add to dataframe
-                # file name | Text 
-        } else
-        {
-                return(print("Conversion Failed"))
-        }
-        return(data_frame_of_textfiles) # unique name..
+ ##################        
 }
 
-
+dummy
 # Tell R what folder contains your PDFs
 dest <- "D:/CPUC_PGE/CPUC_PGE_ETL/PDF_2010"
 # Create a list of PDF file names
 myfiles <- list.files(path = dest, pattern = "pdf",  full.names = TRUE)
-myfiles[1]
-lapply(myfiles[2], function(i) system(paste('"C:/Program Files/xpdf/bin64/pdftotext.exe"', paste0('"', i, '"')), wait = FALSE) )
+myfiles[3]
+lapply(myfiles, function(i) system(paste('"C:/Program Files/xpdf/bin64/pdftotext.exe"', paste0('"', i, '"')), wait = FALSE) ) # Make call to convert files
 
 
 #### Read all text files into data frame, add column removing ".txt" as a key
 library(readr)
-file_names <- c("SB_GT&S_0000001.txt", "SB_GT&S_0000002.txt")
-x <- read_file("PDF_2010/SB_GT&S_0000001.txt")
-y <- read_file("PDF_2010/SB_GT&S_0000002.txt")
-z <- c(x, y)
+getwd() # "D:/CPUC_PGE/CPUC_PGE_ETL"
+setwd("D:/CPUC_PGE/CPUC_PGE_ETL/PDF_2010")
 
-# create data frame of all files, from and to
-x_df <- data.frame(file_names, z)
+# Function to change individual text file into data frame and remove line breaks
+file_convert <- function(file_name) {
+        library(readr) #library to read in files
+        library(tibble) #library to save as tbl_df, aka tibble
+        my_string <- read_file(file = file_name) # read file
+        my_string <- as.character(my_string)
+        my_string <- gsub("\r?\n|\r", " ", my_string) # remove line breaks
+        text_name <- file_name # add variable for file anme
+        data <- data_frame(text_name, my_string) 
+        colnames(data) <- c("File", "Text") 
+        return(data) 
+}
 
-
+# Load multiple files from folder into single table. 
+# Table key is file name, content is email text
+getwd()
+setwd("D:/CPUC_PGE/CPUC_PGE_ETL/PDF_2010")# set to appropriate working directory
+files = list.files(pattern = "*.txt") # list files
+df <- lapply(files, file_convert) %>% bind_rows() #convert to data frame
 
 
 # Download XLS files
--- CPUC_sql.r
+++ CPUC_sql.r
@@ -18,13 +18,17 @@ dbRemoveTable(db, "email_index")
 #                 '-> email_image
 #    person/entity (email, linkedin info, associated send emails, associated receive)
 
-#setwd("./PDF_2010")
+#setwd("./PDF_2010") 
 #setwd("..") #back to home
 #getwd() # D:/CPUC_PGE/CPUC_PGE_ETL
-library(readr)
-my_string <- read_file(file = "./PDF_2010/SB_GT&S_0000001.txt")
-my_string
 
+# my_string <- read_file(file = "SB_GT&S_0000001.txt")
+# my_string <- gsub("\r?\n|\r", " ", my_string) #remove line breaks
+# my_string
+
+#library(tm)
+#text <- read.delim("this is a test for R load.txt", sep = "/t")
+#text_corpus <- Corpus(VectorSource(text), readerControl = list(language = "en"))
 
 # Email_text
 #  email  text  (add from index) subject sender receiver
