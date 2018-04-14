34ea447bba98b87ca59616ddca65ba0c6c4db801
Randy Avalos <randy.joseph.avalos@gmail.com>
2018-04-14 13:46:20 -0700

    Fix Sender Email Split
    Fix Sender Email split naming. Had duplicate variable names that has
    been resolved by changing order of split and naming NA observations.

--- CPUC_ETL.r
+++ CPUC_ETL.r
@@ -1,7 +1,9 @@
 # Download, transform, and load data
 
+setwd("D:/CPUC_PGE/CPUC_repo/CPUC_PGE")
 
 library(dplyr)
+library(tidyr)
 library(RCurl)
 library(data.table)
 library(lubridate)
@@ -18,29 +20,26 @@ base.url <- "ftp://ftp2.cpuc.ca.gov/PG&E20150130ResponseToA1312012Ruling"
 dest_file <- getURL("ftp://ftp2.cpuc.ca.gov/PG&E20150130ResponseToA1312012Ruling/Index/SB_GT&S_Production Metadata.csv") #download file as character
 
 email_index <- fread(dest_file, sep = ",", header = TRUE) #convert to data frame
-
 remove(dest_file) #clean up
 
+# Transform data frame. Make each observation a unique email between sender and receiver pair
+email_index$Subject[email_index$Subject==""] <- paste0("Attachement for email sent on ", as.character(email_index$MasterDate))# observations without a subject assumed to be attachement for email
 
-# Transform data frame
-email_index$Recipient_Name[email_index$Recipient_Name==""] <- "Not Recorded (Not Recorded)" #stand in for missing receipent name value
-email_index$Sender_Name[email_index$Sender_Name==""] <- "Not Recorded (Not Recorded)"
-email_index$Subject[email_index$Subject==""] <- "Not an Email"# observations without a subject assumed to be not emails between users
 email_index$MasterDate <- as.POSIXct(email_index$MasterDate, format = "%m/%d/%Y %H:%M") # convet to date with time
 email_index$Day <- floor_date(email_index$MasterDate, "days") # save date of email in new column
- 
-## Lets take apart the sender email into name and email address
 email_index <- email_index %>%
-        separate(Sender, into = c("Sender_Name", "Sender_Email"), sep = "\\(") # split sender into name and email
+        separate(Sender, into = c("Sender_Name", "Sender_Email"), sep = "\\(") # split sender into name and email into seperate variables/features
 email_index$Sender_Email <- gsub("\\)", "",email_index$Sender_Email) # remove )
+email_index$Sender_Name[email_index$Sender_Name==""] <- "Not Recorded (Not Recorded)"
 
-# ## Break apart recipients, long data format
-# email_index <- email_index %>%
-#         separate_rows(Recipient, sep = ";")
-# email_index <- email_index %>%
-#         separate(Recipient, into = c("Recipient_Name", "Recipient_Email"), sep = "\\(") #split recipient 
-# email_index$Recipient_Email <- gsub("\\)", "", email_index$Recipient_Email) # remove )
-# 
+email_index <- email_index %>%
+        separate_rows(Recipient, sep = ";") # split recipients into unique observations, increases obs from 120,746 to 363,726 obs. Each email pair is a communcation.
+email_index <- email_index %>%
+        separate(Recipient, into = c("Recipient_Name", "Recipient_Email"), sep = "\\(") #split recipient name and email into separate variables
+email_index$Recipient_Email <- gsub("\\)", "", email_index$Recipient_Email) # remove )
+email_index$Recipient_Name[email_index$Recipient_Name==""] <- "Not Recorded (Not Recorded)" #stand in for missing receipent name value
+
+
 # # Remove leading and trailing white space
 # email_index$Sender_Name <- trimws(email_index$Sender_Name, which = "both")
 # email_index$Sender_Email <- trimws(email_index$Sender_Email, which = "both")
